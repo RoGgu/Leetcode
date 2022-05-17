18e1b1b30271e425f39989268facb3c5abf71299
로꾸 <cjswo4577@gmail.com>
2022-05-17 21:59:16 +0900

    Time: 0 ms, Memory: 8.7 MB - LeetHub

--- remove-element/remove-element.cpp
+++ remove-element/remove-element.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) 
     {
-        int cnt = 0;
+        // 현재 인덱스에 값이 제거 해야할 값이 아닌경우에 현재 담아야할 인덱스에 넣는다.
+        // 이러면 제거해야할 값이 나오면 다음값이 덮어씌움
 
-        for (int i = 0; i < nums.size()- cnt; i++)
+        int idx = 0; // 인덱스에 하나씩 넣을때마다 증가
+
+        for (int i = 0; i < nums.size(); i++)
         {
-
-            if (nums[i] == -1)
-            {
-                nums[i] = nums[i + cnt];
-                nums[i + cnt] = -1;
-            }
-
-            while (nums[i] == val)
-            {
-                cnt++;
-                if (i + cnt >= nums.size())
-                    break;
-                int tmp = nums[i + cnt];
-                nums[i + cnt] = -1;
-                nums[i] = tmp;
+            if (nums[i] != val) // 제거해야할 꺼 아니면 덮어씌운다.
+            { 
+                nums[idx] = nums[i];
+                idx++; // 하나 채웠으니 인덱스 증가
             }
         }
 
-        return  nums.size() - cnt;
+        return idx;
     }
 };
