126847484417da51059b3f1d6ed1e6b692bea40f
로꾸 <cjswo4577@gmail.com>
2022-05-29 22:33:43 +0900

    Time: 39 ms (22.05%), Space: 23.3 MB (42.14%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,15 +4,9 @@ public:
     {
         int l = 0;
         int r = s.size() - 1;
-        char tmp;
         while (l<r)
         {
-            tmp = s[l];
-            s[l] = s[r];
-            s[r] = tmp;
-
-            l++;
-            r--;
+            swap(s[l++], s[r--]);
         }
     }
 };
