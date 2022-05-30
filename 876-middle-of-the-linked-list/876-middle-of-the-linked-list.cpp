a367d695fa3fafadbbbe8acfc6c2f42452746859
로꾸 <cjswo4577@gmail.com>
2022-05-30 21:54:36 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (72.24%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -14,7 +14,10 @@ public:
     {
         ListNode *slow = head, *fast = head;
         while (fast && fast->next)
-            slow = slow->next, fast = fast->next->next;
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
         return slow;
     }
 };
