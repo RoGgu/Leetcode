26ebd4371a7242f469266277f1ff00060073b217
로꾸 <cjswo4577@gmail.com>
2022-06-01 16:37:53 +0900

    Time: 47 ms (31.79%), Space: 25 MB (81.71%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,13 +1,15 @@
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) 
     {
-        vector<int> res(nums.size());
-        for (int i = 0; i < nums.size(); i++)
-        {
-            res[ (i+k)%nums.size()] = nums[i];
-        }
+        int size = nums.size();
+        if (k % size == 0) // 사이즈 만큼 돌면 제자리라서 제자리 도는거 다 없앤다.
+            return;
+        k = k % size;
 
-        nums = res;
+        reverse(nums.begin(), nums.end()); // 반대로 다 돌린다.
+        reverse(nums.begin(), nums.begin() + k); // k번 이동한거 까지 뒤집고
+        reverse(nums.begin() + k, nums.end()); // 그다음 k+1번 까지 뒤집는다.
     }
 };
