aa1467f93c883f00b164d732d80e0e27af500691
로꾸 <cjswo4577@gmail.com>
2022-06-09 17:14:21 +0900

    Time: 20 ms (44.46%), Space: 15.7 MB (6.15%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -9,7 +9,7 @@ public:
         {
             if (target == numbers[l] + numbers[r]) // 합친 수가 정답이면 현재 인덱스들 리턴
                 break;
-            else if (target > numbers[l] + numbers[r]) // 합친수가 작으면 l 작은쪽 인덱스 올려줌
+            else if (target > numbers[l] + numbers[r]) // 합친 수가 작으면 l 작은쪽 인덱스 올려줌
                 l++;
             else // 합친수가 크면 r 큰 쪽 인덱스 낮춰줌
                 r--;
