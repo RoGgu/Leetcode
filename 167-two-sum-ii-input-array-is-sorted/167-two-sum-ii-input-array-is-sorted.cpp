8045e727b9c16adff50b5a560169e83e6ca58ed2
로꾸 <cjswo4577@gmail.com>
2022-06-09 17:14:16 +0900

    Time: 23 ms (30.54%), Space: 15.5 MB (93.11%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,4 +1,3 @@
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) 
@@ -8,11 +7,11 @@ public:
 
         while (l < r)
         {
-            if (target == numbers[l] + numbers[r])
+            if (target == numbers[l] + numbers[r]) // 합친 수가 정답이면 현재 인덱스들 리턴
                 break;
-            else if (target > numbers[l] + numbers[r])
+            else if (target > numbers[l] + numbers[r]) // 합친수가 작으면 l 작은쪽 인덱스 올려줌
                 l++;
-            else
+            else // 합친수가 크면 r 큰 쪽 인덱스 낮춰줌
                 r--;
         }
 
