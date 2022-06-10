b460bcc6c20b08de3f6809e921ddf5b66cf07179
로꾸 <cjswo4577@gmail.com>
2022-06-11 00:33:50 +0900

    Time: 18 ms (67.85%), Space: 8.4 MB (59.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) 
     {
-        vector<int> dict(256, -1);
-        int maxLen = 0;
-        int start = -1;
-        for (int i = 0; i != s.length(); i++)
+        vector<int> ch(256, -1);
+        int startIdx = -1; // 카운트 시작한 인덱스 값
+        int maxLenght = 0;
+
+        for (int i = 0; i < s.size(); i++)
         {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+            if (ch[s[i]] > startIdx) // ch가 이미 세팅 즉 앞에서 이미 있다면 그 단어 인덱스를 스타트 위치로 세팅
+                startIdx = ch[s[i]];
+            ch[s[i]] = i; // 발견된 문자에 인덱스를 세팅한다
+
+            maxLenght = max(maxLenght, i - startIdx); // 맥스값 비교해서 세팅
         }
-        return maxLen;
+        return maxLenght;
     }
 };
