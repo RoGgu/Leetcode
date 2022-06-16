4219e45591753da3781ebbe94c33120a11c3e893
로꾸 <cjswo4577@gmail.com>
2022-06-16 14:37:58 +0900

    Time: 209 ms (50.99%), Space: 231.6 MB (19.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -22,13 +22,12 @@ public:
             int len1 = PalindRomeLen(s, i, i); // 홀 수 탐색
             int len2 = PalindRomeLen(s, i, i + 1); // 짝 수 탐색 
             int len = max(len1, len2); 
-            if (len > end - start + 1) 
+            if (len > end - start + 1)  // 처음 세팅된 회전문 길이보다 크지 않으면 세팅 안함
             {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
         }
-        return s.substr(start, end - start +1);
+        return s.substr(start, end - start +1); // 시작 지점, 시작지점 부터 얻어올 길이
     }
-
 };
